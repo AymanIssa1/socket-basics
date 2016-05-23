351692d9b269d6ff31f9b9d64fc230a3714a5d1d
Ayman Adel <aymanadel003@gmail.com>
2016-05-23 10:04:51 +0200

    Add front-end to submit message

--- server.js
+++ server.js
@@ -9,15 +9,16 @@ app.use(express.static(__dirname + '/public'));
 io.on('connection', function (socket) {
     console.log('User Connected via socket.io');
     
-    socket.on('message', function(message) {
+    socket.on('message',function(message) {
        console.log('Message received: ' + message.text);
        
        socket.broadcast.emit('message',message);
     });
     
-    socket.emit('message',{
+    socket.emit('message', {
        text: 'Welcome to the chat application!' 
     });
+    
 });
 
 http.listen(PORT, function() {
