7dc2aacc45d296a979a6900e09b2fb23076f7552
Ayman Adel <aymanadel003@gmail.com>
2016-05-24 06:49:29 +0200

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,23 @@
 var socket = io();
 
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name + " Wants to jons " + room);
+
 socket.on("connect",function () {
    console.log("Connected to socket.io server!"); 
 });
 
 socket.on('message', function(message) {
     var momentTimestamp = moment.utc(message.timestamp);
+    var $message = jQuery('.messages');
     
    console.log('New Message :');
    console.log(message.text);
    
-   jQuery('.messages').append('<p><strong>'+ momentTimestamp.format('h:mma') + ' : </strong>'+ message.text + '</p>');
-   
+   $message.append('<p><strong>'+ message.name + ' ' + momentTimestamp.local().format('h:mma') + '</strong></p>');
+   $message.append('<p>' + message.text + '</p>')
 });
 
 // Handles submitting of new message
@@ -23,7 +29,8 @@ $form.on('submit', function(event) {
    var $message = $form.find('input[name=message]');
    
    socket.emit('message', {
-      text: $message.val()
+       name: name,
+       text: $message.val()
    });
    
    $message.val('');
